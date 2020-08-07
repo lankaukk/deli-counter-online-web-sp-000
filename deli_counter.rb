79e3e22ecebe0af9dd9a9037db73f76f1b8ded6f
McKayla Lankau <lankaukk@yahoo.com>
2020-08-07 02:58:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 
 def take_a_number(katz_deli, name)
-  counter = 1
+  
   katz_deli.each do |person_in_line|
     puts "Welcome, #{person_in_line}. You are number #{counter} in line."
     counter += 1
