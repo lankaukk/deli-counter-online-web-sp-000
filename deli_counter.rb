72134b2dc7a6d2bdfdced06ded5acf1fccde3f96
McKayla Lankau <lankaukk@yahoo.com>
2020-08-07 02:36:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli.size == 0
-    puts "The line is currently empty."
-  elsif katz_deli.size != 0 
-    line = ["The line is currently: "]
-    katz_deli.each_with_index.map do |customer, index|
-      line << "#{index+1}. #{customer}"
+    if katz_deli.size == 0
+      puts "The line is currently empty."
+    elsif katz_deli.size != 0 
+      line = ["The line is currently: "]
+      katz_deli.each_with_index.map do |customer, index|
+        line << "#{index+1}. #{customer}"
+      end
+      line
     end
-    line
-  end
 end
 
 
