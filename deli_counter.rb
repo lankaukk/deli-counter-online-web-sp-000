319b39a4a3687d0b1bd34b6f83cf5c948563997b
McKayla Lankau <lankaukk@yahoo.com>
2020-08-07 01:54:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif katz_deli.size != 0 
+    counter = 1
+    katz_deli.each do |customer|
+      puts "The line is currently: #{counter}. #{customer}"
+    counter += 1
+    end
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  counter = 1
+  puts "Welcome, #{name}. You are number #{counter} in line."
+  counter += 1
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
+
