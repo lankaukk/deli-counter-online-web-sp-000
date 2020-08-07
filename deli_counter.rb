f85fdffd7f17cff82c55d10821b13af43e87030a
McKayla Lankau <lankaukk@yahoo.com>
2020-08-07 02:31:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   elsif katz_deli.size != 0 
+    line = []
     katz_deli.each_with_index.map do |customer, index|
       puts "The line is currently: #{index+1}. #{customer}"
     end
